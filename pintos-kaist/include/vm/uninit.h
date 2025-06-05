d8e6159e1fc379657c95f65254da854b43d0ceae
BJJ <a01072004146@gmail.com>
2025-06-05 21:31:05 +0900

    feat(project3): anon, lazy load 구현

--- pintos-kaist/include/vm/uninit.h
+++ pintos-kaist/include/vm/uninit.h
@@ -5,20 +5,21 @@
 struct page;
 enum vm_type;
 
-typedef bool vm_initializer (struct page *, void *aux);
+typedef bool vm_initializer(struct page *, void *aux);
 
 /* Uninitlialized page. The type for implementing the
  * "Lazy loading". */
-struct uninit_page {
+struct uninit_page
+{
 	/* Initiate the contets of the page */
-	vm_initializer *init;
-	enum vm_type type;
-	void *aux;
+	vm_initializer *init; /* 페이지 폴트 시 호출할 초기화 콜백 함수 */
+	enum vm_type type;	  /* 최종 페이지 타입(예: VM_FILE, VM_ANON 등) */
+	void *aux;			  /* init 함수에 넘겨 줄 추가 파라미터 */
 	/* Initiate the struct page and maps the pa to the va */
-	bool (*page_initializer) (struct page *, enum vm_type, void *kva);
+	bool (*page_initializer)(struct page *, enum vm_type, void *kva);
 };
 
-void uninit_new (struct page *page, void *va, vm_initializer *init,
-		enum vm_type type, void *aux,
-		bool (*initializer)(struct page *, enum vm_type, void *kva));
+void uninit_new(struct page *page, void *va, vm_initializer *init,
+				enum vm_type type, void *aux,
+				bool (*initializer)(struct page *, enum vm_type, void *kva));
 #endif
